4dd1b748893e6934eb0b498e9f96bac1308763ee
Matthieu Oger <moger@pixelnest.io>
2015-02-28 04:36:37 +0100

    Update osx_defaults.

---
+++ osx_defaults.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Set the save dialog to be in extended mode by default.
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Set the iCloud-using apps to not save to iCloud by default.
+# Courtesy of @room34, http://blog.room34.com/archives/5098
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Open a new document in TextEdit (& other iCloud-backed app) directly,
+# instead of presenting the document picker.
+defaults write -g NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
