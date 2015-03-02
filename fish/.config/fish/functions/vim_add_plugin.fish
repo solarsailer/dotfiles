42324402ca60a214b995b9288e18939da54e0897
Matthieu Oger <moger@pixelnest.io>
2015-03-02 15:02:17 +0100

    Fish: temp function.

---
+++ fish/.config/fish/functions/vim_add_plugin.fish
@@ -0,0 +1,9 @@
+# Installation:
+#
+# $ cd ~/.vim
+# $ git submodule init
+# $ git submodule add git://github.com/owner/repo.git bundle/repo
+# $ git commit -m 'Vim: add repo'
+# $ git push
+
+#TODO
