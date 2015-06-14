8186605b7b29c71589a512e3a010ce4cef1224a2
Matthieu Oger <moger@pixelnest.io>
2015-06-14 02:09:21 +0200

    Spacemacs: add personal layer + TODO.

---
+++ spacemacs/.config/spacemacs/README.md
@@ -0,0 +1,11 @@
+# TODO
+
+* Remove the tildes.
+* Put the OSX right-meta to none in the personal layer.
+* Change the wrapping, indent & whitespace cleaning.
+* Modify powerline (minimal).
+* j & k for visual lines and not logical lines.
+* Remove the -- INSERT -- (statusline in Vim).
+* Change the title bar in the OSX app.
+* Remove the startup page.
+* 1px at a time scrolling.
---
+++ spacemacs/.config/spacemacs/personal/extensions.el
@@ -0,0 +1,31 @@
+;;; extensions.el --- personal Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq personal-pre-extensions
+      '(
+        ;; pre extension personals go here
+        ))
+
+(setq personal-post-extensions
+      '(
+        ;; post extension personals go here
+        ))
+
+;; For each extension, define a function personal/init-<extension-personal>
+;;
+;; (defun personal/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
