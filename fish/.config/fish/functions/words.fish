06056622b404ee1d39f7f70d346d60744943be74
Matthieu Oger <moger@pixelnest.io>
2015-08-07 18:36:31 +0200

    Fish: cosmetic.

--- fish/.config/fish/functions/words.fish
+++ fish/.config/fish/functions/words.fish
@@ -1,6 +1,7 @@
 function words
   if test (count $argv) -gt 0
     echo -n "Number of words: "
+    
     # Only print the result of the last line (the sum).
     wc -w $argv | awk 'END{print $1}'
   else
