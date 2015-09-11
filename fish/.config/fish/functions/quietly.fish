597fe05c72f6694afb2a6faa453f41274209206d
Matthieu Oger <moger@pixelnest.io>
2015-09-11 16:51:15 +0200

    Fish: add quietly function.

---
+++ fish/.config/fish/functions/quietly.fish
@@ -0,0 +1,6 @@
+# Made by Steve Losh:
+# https://bitbucket.org/sjl/dotfiles/src/0f635ab417a76971eb7533be22d2cde41140667a/fish/functions/quietly.fish
+
+function quietly -d "Run a command and hide the output"
+  eval $argv >/dev/null ^/dev/null
+end
