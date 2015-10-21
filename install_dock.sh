ed853e632dedb52678e5c8eaff6970687340ee94
Matthieu Oger <moger@pixelnest.io>
2015-10-21 15:51:04 +0200

    Install/Dock: add iTerm & Downloads.

--- install_dock.sh
+++ install_dock.sh
@@ -6,3 +6,7 @@ dockutil --remove all
 # Re-add things.
 dockutil --add "/Applications/Google Chrome.app"
 dockutil --add "/Applications/Slack.app"
+dockutil --add "/Applications/Messages.app"
+dockutil --add "/Applications/iTerm.app"
+
+dockutil --add '~/Downloads' --view grid --display stack
