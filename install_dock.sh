962deb9ad38743444a622743759703247f196421
Matthieu Oger <moger@pixelnest.io>
2015-11-20 09:56:30 +0100

    Scripts/Dock: add Spotify & re-order.

--- install_dock.sh
+++ install_dock.sh
@@ -5,9 +5,10 @@ dockutil --remove all
 
 # Re-add things.
 dockutil --add "/Applications/Google Chrome.app"
+dockutil --add "/Applications/Spotify.app"
+dockutil --add "/Applications/Fantastical 2.app"
 dockutil --add "/Applications/Slack.app"
 dockutil --add "/Applications/Messages.app"
-dockutil --add "/Applications/Fantastical 2.app"
 dockutil --add "/Applications/Notes.app"
 dockutil --add "/Applications/iTerm.app"
 
