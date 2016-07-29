0d8d7c7395db910aceca6f51e4b80c001dca68ca
Matthieu Oger <moger@pixelnest.io>
2016-07-29 15:39:08 +0200

    Add Markdown Services

---
+++ install_services.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# -------------------------------------------------------
+# Markdown tools.
+# -------------------------------------------------------
+
+# Get the package from:
+# http://brettterpstra.com/projects/markdown-service-tools/
+# Then, move the services to `~/Library/Services`.
