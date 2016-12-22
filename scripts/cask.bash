61476d742ee24bf6e6e6d0e4b32aa6fa51e2ae42
Matthieu Oger <moger@pixelnest.io>
2016-12-22 17:28:56 +0100

    Scripts: add apps to cask

--- scripts/cask.bash
+++ scripts/cask.bash
@@ -35,18 +35,28 @@ brew cask install markdown-service-tools
 
 echo "Cask> Installing apps…"
 
-# brew cask install google-chrome
-# brew cask install dropbox
-# brew cask install 1password
-# brew cask install iterm2
-# brew cask install slack
-# brew cask install alfred
-# brew cask install droplr
-# brew cask install spotify
-# brew cask install bartender
-# brew cask install vlc
-# brew cask install imageoptim
-# brew cask install paparazzi!
-# brew cask install virtualbox
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install firefox
+brew cask install 1password
+brew cask install atom
+brew cask install iterm2
+brew cask install sketch
+brew cask install origami
+brew cask install slack
+brew cask install alfred
+brew cask install spotify
+brew cask install taskpaper
+brew cask install dash
+brew cask install vlc
+brew cask install bartender
+brew cask install paparazzi
+# brew cask install moom
+brew cask install hazel
+brew cask install choosy
+brew cask install daisydisk
+brew cask install imageoptim
+brew cask install namechanger
+brew cask install virtualbox
 
 echo "Cask> Done.\n"
