c85e8c16f0869c05d38920107cac279bd676c1c4
Matthieu Oger <moger@pixelnest.io>
2016-12-22 17:13:41 +0100

    Scripts: remove services

--- scripts/services.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# -------------------------------------------------------
-# Markdown tools.
-# -------------------------------------------------------
-
-# Get the package from:
-# http://brettterpstra.com/projects/markdown-service-tools/
-# Then, move the services to `~/Library/Services`.
