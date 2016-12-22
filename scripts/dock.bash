3ceec4938fa819db729c6f4f4955b2b1a63d74fd
Matthieu Oger <moger@pixelnest.io>
2016-12-22 16:03:19 +0100

    Scripts: clean dock

--- scripts/dock.bash
+++ scripts/dock.bash
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-# Remove everything.
+# --------------------------------------------------------------
+# Dock.
+# --------------------------------------------------------------
+
+echo "Dock> Configuring…"
+
 dockutil --remove all
 
 # Re-add things.
+dockutil --add "/Applications/Safari.app"
 dockutil --add "/Applications/Google Chrome.app"
 dockutil --add "/Applications/Spotify.app"
 dockutil --add "/Applications/Fantastical 2.app"
+dockutil --add "/Applications/Preview.app"
 dockutil --add "/Applications/Slack.app"
 dockutil --add "/Applications/Messages.app"
-dockutil --add "/Applications/Notes.app"
+dockutil --add "/Applications/TaskPaper.app"
+dockutil --add "/Applications/Sketch.app"
 dockutil --add "/Applications/iTerm.app"
+dockutil --add '' --type spacer --section apps
 
-dockutil --add '~/Downloads' --view grid --display stack
+dockutil --add '~/Downloads' --sort datemodified --view auto --display stack
+
+echo "Dock> Done."
