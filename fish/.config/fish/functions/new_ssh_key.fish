872c6abbe9806ab20e7e010a17f358a0ca9f7991
Matthieu Oger <moger@pixelnest.io>
2017-02-08 19:38:45 +0100

    Fish: update new_ssh_key with named arguments

--- fish/.config/fish/functions/new_ssh_key.fish
+++ fish/.config/fish/functions/new_ssh_key.fish
@@ -1,3 +1,7 @@
-function new_ssh_key -d "new_ssh_key destination name"
-  ssh-keygen -t rsa -b 4096 -C $argv[2] -f $argv[1]
+# Create a new SSH key.
+#
+# Example:
+#   new_ssh_key 'path/to/test_key' 'krokmou@berk.com'
+function new_ssh_key -a filename email -d "new_ssh_key path/to/filename email@domain.com"
+  ssh-keygen -t rsa -b 4096 -C $email -f $filename
 end
