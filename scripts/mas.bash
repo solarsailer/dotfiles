d00371a3f8b00fe4ade7f4bfb73e87dbdacc2933
Matthieu Oger <moger@pixelnest.io>
2017-03-27 10:10:47 +0200

    MAS: clean and add help

--- scripts/mas.bash
+++ scripts/mas.bash
@@ -2,16 +2,18 @@
 
 echo "MAS> Installing Mac App Store apps…"
 
-mas install 497799835 # Xcode
-mas install 975937182 # Fantastical 2
-mas install 413965349 # Soulver
-mas install 924691732 # Flare 2
-mas install 419330170 # Moom
-mas install 439697913 # Icon Slate
+# FInd app_id with `mas list`.
+
+mas install 497799835  # Xcode
+mas install 975937182  # Fantastical 2
+mas install 413965349  # Soulver
+mas install 924691732  # Flare 2
+mas install 419330170  # Moom
+mas install 439697913  # Icon Slate
 mas install 1107421413 # 1Blocker
-mas install 880001334 # Reeder
-mas install 557168941 # Tweetbot
-mas install 735066709 # Pixel Winch
+mas install 880001334  # Reeder
+mas install 557168941  # Tweetbot
+mas install 735066709  # Pixel Winch
 mas install 1166066070 # Bumpr
 
 echo "MAS> Done."
