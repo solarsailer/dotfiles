4dc4c9187b2e717cb5b8df44200f8606eb1df710
Matthieu Oger <moger@pixelnest.io>
2017-06-12 10:32:08 +0200

    MAS: add Things & Annotate

--- scripts/mas.bash
+++ scripts/mas.bash
@@ -6,6 +6,7 @@ echo "MAS> Installing Mac App Store apps…"
 
 mas install 497799835  # Xcode
 mas install 975937182  # Fantastical 2
+mas install 904280696  # Things 3
 mas install 413965349  # Soulver
 mas install 924691732  # Flare 2
 mas install 419330170  # Moom
@@ -14,6 +15,7 @@ mas install 1107421413 # 1Blocker
 mas install 880001334  # Reeder
 mas install 557168941  # Tweetbot
 mas install 735066709  # Pixel Winch
+mas install 918207447  # Annotate
 mas install 1166066070 # Bumpr
 
 echo "MAS> Done."
