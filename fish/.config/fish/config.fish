3490213fa6fa06070c53796968429c3681933d4b
Matthieu Oger <moger@pixelnest.io>
2017-07-07 12:57:41 +0200

    Fish: add LS_COLORS for Linux-based commands

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,12 @@ set --export GIT_EDITOR "vim"
 
 # Colors.
 set --export CLICOLOR 1
-set --export LSCOLORS GxFxCxDxBxegedabagaced
+
+# LSCOLORS is for BSD-based systems.
+# LS_COLORS is for Linux-based systems.
+# Use: https://geoff.greer.fm/lscolors/
+set --export LSCOLORS "GxFxCxDxBxegedabagaced"
+set --export LS_COLORS "di=1;36:ln=1;35:so=1;32:pi=1;33:ex=1;31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=34;43"
 
 # --------------------------------------------------------------
 # Path
