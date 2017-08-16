60922226cc668f51c20cb60bfedecca0d14156e3
Matthieu Oger <moger@pixelnest.io>
2017-08-16 22:42:59 +0200

    Dock: update

--- scripts/dock.bash
+++ scripts/dock.bash
@@ -13,13 +13,9 @@ dockutil --add "/Applications/Safari.app"
 dockutil --add "/Applications/Google Chrome.app"
 dockutil --add "/Applications/Spotify.app"
 dockutil --add "/Applications/Fantastical 2.app"
-dockutil --add "/Applications/Preview.app"
-dockutil --add "/Applications/Slack.app"
 dockutil --add "/Applications/Messages.app"
-dockutil --add "/Applications/TaskPaper.app"
-dockutil --add "/Applications/Sketch.app"
+dockutil --add "/Applications/Things3.app"
 dockutil --add "/Applications/iTerm.app"
-dockutil --add '' --type spacer --section apps
 
 dockutil --add '~/Downloads' --sort datemodified --view auto --display stack
 
