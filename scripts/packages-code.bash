3eed50209fe4784efd87d598bbbe56047a2d0bcd
Matthieu Oger <moger@pixelnest.io>
2017-11-17 00:14:35 +0100

    Code: add extensions list

---
+++ scripts/packages-code.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------------------------
+# Visual Studio Code.
+# --------------------------------------------------------------
+
+echo "Code> Installing…"
+
+code --install-extension file-icons.file-icons
+code --install-extension Unity.unity-debug
+code --install-extension ms-vscode.csharp
+
+echo "Code> Done."
