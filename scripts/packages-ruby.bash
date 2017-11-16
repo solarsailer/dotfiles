458bca5f28f735c2436b535d1d7cee0bcc0de40f
Matthieu Oger <moger@pixelnest.io>
2017-11-17 00:31:03 +0100

    Scripts: complete ruby

--- scripts/packages-ruby.bash
+++ scripts/packages-ruby.bash
@@ -1,15 +1,36 @@
 #!/usr/bin/env bash
 
+# Get the list: `rbenv install --list`
+RUBY_VERSION=2.4.2
+
 # -------------------------------------------------------
 # rbenv.
 # -------------------------------------------------------
 
-# TODO rbenv
+RESULT=$(rbenv versions | awk "/$RUBY_VERSION/ {print \"true\"}")
+
+# Install if not present.
+if [ "$RESULT" != "true" ]; then
+  echo "Ruby> Installing…"
+  rbenv install $RUBY_VERSION
+  echo "Ruby> Done."
+else
+  echo "Ruby> $RUBY_VERSION already installed."
+fi
+
+echo "Ruby> Setting as global…"
+
+rbenv global $RUBY_VERSION
+rbenv rehash
+
+echo "Ruby> Done."
 
 # -------------------------------------------------------
 # Gems.
 # -------------------------------------------------------
 
-# TODO Gems
+echo "Ruby> Installing gems…"
+
+gem install bundler
 
 echo "Ruby> Done."
