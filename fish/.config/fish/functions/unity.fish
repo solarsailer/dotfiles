1d3e5b510a3e86ad2b122a49ca4b7a5f557cdd9b
Matthieu Oger <moger@pixelnest.io>
2018-09-04 11:42:55 +0200

    Fish: improve unity command to open projects directly

--- fish/.config/fish/functions/unity.fish
+++ fish/.config/fish/functions/unity.fish
@@ -6,9 +6,18 @@ function unity
     return 1
   end
 
+  set -l current_path (pwd)
   set -l unity_version (cat $project_settings | awk '/EditorVersion/ {print $2}')
   set -l unity_app "/Applications/Unity $unity_version/Unity $unity_version.app"
+  set -l unity_exe "/Applications/Unity\ $unity_version/Unity\ $unity_version.app/Contents/MacOS/Unity"
 
   echo "Using Unity $unity_version…"
-  open $unity_app
+
+  # If -l/--launcher argument is provided, just open the Unity app.
+  # Otherwise, open the project without using the launcher.
+  if test "$argv[1]" = "--launcher" -o "$argv[1]" = "-l"
+    open "$unity_app"
+  else
+    bash -c "$unity_exe -projectPath $current_path &"
+  end
 end
