52c8ea6ac1253a16bc5d3975e1dfddadd51c2542
Matthieu Oger <moger@pixelnest.io>
2019-02-09 17:26:19 +0100

    Code: add packages

--- scripts/packages-code.bash
+++ scripts/packages-code.bash
@@ -7,15 +7,18 @@
 echo "Code> Installing…"
 
 code --install-extension file-icons.file-icons
-code --install-extension Unity.unity-debug
-code --install-extension ms-vscode.csharp
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension esbenp.prettier-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension pnp.polacode
-code --install-extension cmstead.jsrefactor
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension eamodio.gitlens
-code --install-extension peterjausovec.vscode-docker
+code --install-extension alefragnani.Bookmarks
+code --install-extension dbaeumer.vscode-eslint
+
+# code --install-extension Unity.unity-debug
+# code --install-extension ms-vscode.csharp
+# code --install-extension cmstead.jsrefactor
+# code --install-extension eamodio.gitlens
+# code --install-extension peterjausovec.vscode-docker
 
 echo "Code> Done."
