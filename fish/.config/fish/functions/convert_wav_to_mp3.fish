bcc9f556a6bede0f07b35d081d73993f9e62a64a
Matthieu Oger <moger@pixelnest.io>
2019-08-07 20:44:20 +0200

    Fish: fix convert_wav_to_mp3

--- fish/.config/fish/functions/convert_wav_to_mp3.fish
+++ fish/.config/fish/functions/convert_wav_to_mp3.fish
@@ -1,5 +1,5 @@
 # Example:
-# $ wav_to_mp3 Supermassive.wav Supermassive.mp3
-function wav_to_mp3 -d "Convert a WAV to an high quality MP3"
+# $ convert_wav_to_mp3 Supermassive.wav Supermassive.mp3
+function convert_wav_to_mp3 -d "Convert a WAV to an high quality MP3"
   lame -b 320 -h $argv
 end
