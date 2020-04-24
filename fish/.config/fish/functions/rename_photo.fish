d652c2c65dedcd19c7df89db91b957cfd596f258
Matthieu Oger <moger@pixelnest.io>
2020-04-24 20:04:53 +0200

    Fish: handle suffix in rename_photo function

--- fish/.config/fish/functions/rename_photo.fish
+++ fish/.config/fish/functions/rename_photo.fish
@@ -10,19 +10,33 @@ function rename_photo -d "Rename a photo to a specific format"
       echo -n $file
       set_color normal
 
+      # Change the file modified date with the content date.
       exiftool -q "-DateTimeOriginal>FileModifyDate" $file
 
+      # Get the date, model and extension.
       set date (exiftool -d "%Y-%m-%d" -DateTimeOriginal -S -s $file)
-      set time (exiftool -d "%H%M%S" -DateTimeOriginal -S -s $file)
       set model (exiftool -Model -S -s $file | sed 's/ /-/g')
       set extension (echo $file | awk -F. '{print $NF}')
+
+      # Try to find a 4 digits suffix in the filename.
+      set suffix (echo $file | grep -ioE '([0-9]{4})\.(png|dng|raf|jpg|jpeg|heic|heif|tiff)$' | awk -F. '{print $1}')
+
+      # As a backup, create a pseudo-unique string from the time and a random number.
+      set time (exiftool -d "%H%M%S" -DateTimeOriginal -S -s $file)
       set random (random 1000 9999)
 
-      mv -n $file $date"_"$model"_"$time$random.$extension
+      # Use suffix if possible, otherwise, create a time and random number combination.
+      if test -n "$suffix"
+        set -g end (echo $suffix.$extension)
+      else
+        set -g end (echo $time$random.$extension)
+      end
+
+      mv -n $file $date"_"$model"_"$end
 
       echo -n " → "
       set_color -o green
-      echo $date"_"$model"_"$time$random.$extension
+      echo $date"_"$model"_"$end
       set_color normal
     else
       set_color red
