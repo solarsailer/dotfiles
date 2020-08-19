308de2f573c4fde9a9e7c03bad6bbcafaabfe72d
Matthieu Oger <moger@pixelnest.io>
2020-08-20 01:24:16 +0200

    Yarn: refresh script

--- scripts/packages-yarn.bash
+++ scripts/packages-yarn.bash
@@ -6,29 +6,24 @@
 
 echo "Yarn> Installing global packages…"
 
-yarn global add create-react-app
-
-yarn global add webpack
-yarn global add webpack-dev-server
-yarn global add brunch
-yarn global add gulp
-yarn global add parcel-bundler
-
-yarn global add jest
+# Dev.
+#yarn global add webpack
+#yarn global add webpack-dev-server
+#yarn global add brunch
+#yarn global add gulp
+#yarn global add parcel-bundler
 
 # Tools.
+yarn global add tldr
+yarn global add jest
+yarn global add prettier
 yarn global add serve
 yarn global add browser-sync
-yarn global add prettier
-yarn global add tldr
 yarn global add fkill-cli
-yarn global add pkg # node packager
-yarn global add @zeit/ncc # node compiler
-yarn global add np # npm publish, simplified
-yarn global add matcher-cli
-yarn global add emma-cli
 yarn global add speed-test
-yarn global add sharp-cli
 yarn global add tagui # visual automation
+#yarn global add np # npm publish, simplified
+#yarn global add pkg # node packager
+#yarn global add @zeit/ncc # node compiler
 
 echo "Yarn> Done."
