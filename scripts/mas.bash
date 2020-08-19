ccdf4aea699aeb4c1cff57b3a6de41694ac6d538
Matthieu Oger <moger@pixelnest.io>
2020-08-20 01:19:46 +0200

    MAS: refresh script

--- scripts/mas.bash
+++ scripts/mas.bash
@@ -5,15 +5,13 @@ echo "MAS> Installing Mac App Store apps…"
 # FInd app_id with `mas list`.
 
 mas install 497799835  # Xcode
-mas install 975937182  # Fantastical 2
-mas install 904280696  # Things 3
+mas install 975937182  # Fantastical
+mas install 904280696  # Things
 mas install 747648890  # Telegram
-mas install 924691732  # Flare 2
-# mas install 419330170  # Moom
+mas install 924691732  # Flare
 mas install 439697913  # Icon Slate
 mas install 1107421413 # 1Blocker
 mas install 880001334  # Reeder
-mas install 557168941  # Tweetbot
 mas install 735066709  # Pixel Winch
 mas install 1287239339 # Color Slurp
 mas install 918207447  # Annotate
@@ -24,8 +22,10 @@ mas install 409183694  # Keynote
 mas install 409201541  # Pages
 mas install 409203825  # Numbers
 mas install 1351639930 # Gifski
-mas install 824171161  # Affinity Designer
-# mas install 405399194  # Kindle
-mas install 1491071483 # Tot
+#mas install 824171161  # Affinity Designer
+#mas install 557168941  # Tweetbot
+#mas install 1491071483 # Tot
+#mas install 405399194  # Kindle
+#mas install 419330170  # Moom
 
 echo "MAS> Done."
