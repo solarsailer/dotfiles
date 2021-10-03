b8f17ea18496262219b9215c04e09902fb1f7886
Matthieu Oger <moger@pixelnest.io>
2021-10-03 23:03:14 +0200

    Scripts: start cleaning mas

--- scripts/mas.bash
+++ scripts/mas.bash
@@ -4,26 +4,38 @@ echo "MAS> Installing Mac App Store apps…"
 
 # FInd app_id with `mas list`.
 
+mas install 409183694  # Keynote
+mas install 409201541  # Pages
+mas install 409203825  # Numbers
 mas install 497799835  # Xcode
 mas install 975937182  # Fantastical
 mas install 904280696  # Things
 mas install 747648890  # Telegram
 mas install 439697913  # Icon Slate
-mas install 1107421413 # 1Blocker
 mas install 880001334  # Reeder
 mas install 735066709  # Pixel Winch
 mas install 1289583905 # Pixelmator Pro
 mas install 944693506  # Fileloupe
-mas install 409183694  # Keynote
-mas install 409201541  # Pages
-mas install 409203825  # Numbers
 mas install 1287239339 # Color Slurp
 mas install 1351639930 # Gifski
-mas install 924691732  # Flare
-mas install 424389933  # Final Cut Pro
-mas install 1278508951 # Trello
 mas install 671595950  # Touch Retouch
 mas install 1522267256 # Shareful
+
+echo "MAS> Done."
+
+# -------------------------------------------------------------
+# Optional.
+# -------------------------------------------------------------
+
+# mas install 924691732  # Flare
+# mas install 424389933  # Final Cut Pro
+
+# -------------------------------------------------------------
+# Archive.
+# -------------------------------------------------------------
+
+# mas install 1107421413 # 1Blocker
+# mas install 1278508951 # Trello
 # mas install 918207447  # Annotate
 # mas install 1254981365 # Contrast
 # mas install 824171161  # Affinity Designer
@@ -31,5 +43,3 @@ mas install 1522267256 # Shareful
 # mas install 1491071483 # Tot
 # mas install 405399194  # Kindle
 # mas install 419330170  # Moom
-
-echo "MAS> Done."
