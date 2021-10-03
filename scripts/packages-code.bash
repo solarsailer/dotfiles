839db790fa4b094a1149ec568e043a5d0791e948
Matthieu Oger <moger@pixelnest.io>
2021-10-03 10:41:24 +0200

    Code: reorder extensions

--- scripts/packages-code.bash
+++ scripts/packages-code.bash
@@ -6,14 +6,14 @@
 
 echo "Code> Installing…"
 
+code --install-extension formulahendry.auto-rename-tag # Still necessary despite VSCode own implementation
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension file-icons.file-icons
+code --install-extension bmalehorn.vscode-fish
 code --install-extension pnp.polacode
 code --install-extension esbenp.prettier-vscode
 code --install-extension jpoissonnier.vscode-styled-components
-code --install-extension formulahendry.auto-rename-tag # Still necessary despite VSCode own implementation
-code --install-extension bmalehorn.vscode-fish
 
 # code --install-extension Unity.unity-debug
 # code --install-extension ms-vscode.csharp
