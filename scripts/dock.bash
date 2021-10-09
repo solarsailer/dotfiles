dd1ce8926ab68c7248a1ac083c7aff1631839e0c
Matthieu Oger <moger@pixelnest.io>
2021-10-10 00:24:17 +0200

    Dock: add apps

--- scripts/dock.bash
+++ scripts/dock.bash
@@ -15,6 +15,8 @@ dockutil --add "/Applications/Spotify.app"
 dockutil --add "/Applications/Fantastical 2.app"
 dockutil --add "/Applications/Messages.app"
 dockutil --add "/Applications/Things3.app"
+dockutil --add "/Applications/Craft.app"
+dockutil --add "/Applications/Notes.app"
 dockutil --add "/Applications/iTerm.app"
 
 dockutil --add '~/Downloads' --sort datemodified --view auto --display stack
