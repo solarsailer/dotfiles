4193eb90fef5d7d97a10c7c1b2b08674d92d36bf
Matthieu Oger <moger@pixelnest.io>
2021-10-10 01:45:37 +0200

    Cask: clean and remove WhatsApp

--- scripts/cask.bash
+++ scripts/cask.bash
@@ -1,103 +1,86 @@
 #!/usr/bin/env bash
 
 # --------------------------------------------------------------
-# Installation.
-# --------------------------------------------------------------
-
-echo "Cask> Tapping…"
-
-if [[ `brew tap` == *"caskroom/cask"* ]]; then
-  echo "Cask already tapped."
-else
-  brew tap caskroom/cask
-fi
-
-# --------------------------------------------------------------
 # Casks.
 # --------------------------------------------------------------
 
-echo "Cask> Installing plugins…"
-
-brew cask install ttscoff-mmd-quicklook # Alternative: qlmarkdown
-# brew cask install qlcolorcode # Does not work anymore
-# brew cask install qlstephen # Does not work anymore
-brew cask install quicklook-csv
-brew cask install quicklook-json
-brew cask install wkhtmltopdf
-# brew cask install colorpicker-developer
-
-echo "Cask> Installing packages…"
-
-brew cask install vagrant
-brew cask install docker
-# brew cask install dotnet-sdk
-
-echo "Cask> Installing services…"
-
-brew cask install markdown-service-tools
-
 echo "Cask> Installing apps…"
 
-# Essential.
-brew cask install dropbox
-brew cask install 1password
-brew cask install alfred
-brew cask install spotify
-brew cask install google-chrome
-brew cask install firefox
+# Essentials.
+brew install --cask dropbox
+brew install --cask 1password
+brew install --cask alfred
+brew install --cask spotify
+brew install --cask google-chrome
+brew install --cask firefox
 
 # Code.
-brew cask install visual-studio-code
-brew cask install dash
-brew cask install gitup
-brew cask install iterm2
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask gitup
+brew install --cask dash
 
 # Design.
-brew cask install figma
-brew cask install sketch
-brew cask install principle
-#brew cask install origami-studio
-
-# Communication.
-brew cask install slack
-brew cask install whatsapp
-# brew cask install caprine
-# brew cask install skype
-# brew cask install discord
+brew install --cask figma
+brew install --cask sketch
+brew install --cask principle
 
 # Others.
-brew cask install soulver
-brew cask install taskpaper
-brew cask install deckset
-brew cask install iina
-brew cask install paparazzi
-brew cask install hazel
-brew cask install daisydisk
-brew cask install imageoptim
-brew cask install namechanger
-brew cask install selfcontrol
-brew cask install kap
-brew cask install hyperswitch
-brew cask install vanilla
-brew cask install choosy
-brew cask install phoenix
-brew cask install bunch
-# brew cask install bartender
-# brew cask install customshortcuts
-# brew cask install turbo-boost-switcher
-# brew cask install vlc
-# brew cask install numi
-# brew cask install platypus
-# brew cask install gifrocket
+brew install --cask soulver
+brew install --cask iina
+brew install --cask daisydisk
+brew install --cask imageoptim
+brew install --cask namechanger
+brew install --cask carbon-copy-cloner
 
-# Tools.
-# brew cask install fastlane
+# Developers.
+brew install --cask vagrant
+brew install --cask docker
 
-# Might fail with SIP.
-brew cask install virtualbox
+# Plugins.
+brew install --cask quicklook-json
 
 echo "Cask> Done."
 
 # -------------------------------------------------------------
 # Archive.
 # -------------------------------------------------------------
+
+# brew install --cask virtualbox
+
+# brew install --cask slack
+# brew install --cask caprine
+# brew install --cask skype
+# brew install --cask discord
+
+# brew install --cask fastlane
+# brew install --cask dotnet-sdk
+
+# brew install --cask taskpaper
+# brew install --cask hazel
+# brew install --cask bunch
+# brew install --cask paparazzi
+# brew install --cask kap
+# brew install --cask hyperswitch
+# brew install --cask phoenix
+# brew install --cask choosy
+# brew install --cask vanilla
+# brew install --cask selfcontrol
+# brew install --cask deckset
+# brew install --cask bartender
+# brew install --cask customshortcuts
+# brew install --cask turbo-boost-switcher
+# brew install --cask vlc
+# brew install --cask numi
+# brew install --cask platypus
+# brew install --cask gifrocket
+# brew install --cask origami-studio
+
+# brew install --cask wkhtmltopdf
+# brew install --cask quicklook-csv
+# brew install --cask markdown-service-tools
+# brew install --cask qlmarkdown             # Does not work anymore
+# brew install --cask ttscoff-mmd-quicklook  # Does not work anymore
+# brew install --cask qlcolorcode            # Does not work anymore
+# brew install --cask qlstephen              # Does not work anymore
+# brew install --cask colorpicker-developer  # Does not work anymore
