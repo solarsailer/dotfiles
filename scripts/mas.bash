9188c4a64f9715a342d118dbfec8f82d4997f4e3
Matthieu Oger <moger@pixelnest.io>
2021-10-10 01:45:27 +0200

    MAS: rework and add WhatsApp and Lungo

--- scripts/mas.bash
+++ scripts/mas.bash
@@ -4,22 +4,19 @@ echo "MAS> Installing Mac App Store apps…"
 
 # FInd app_id with `mas list`.
 
+mas install 975937182  # Fantastical
+mas install 904280696  # Things
+mas install 1487937127 # Craft
+mas install 747648890  # Telegram
+mas install 1147396723 # WhatsApp
+mas install 497799835  # Xcode
 mas install 409183694  # Keynote
 mas install 409201541  # Pages
 mas install 409203825  # Numbers
-mas install 497799835  # Xcode
-mas install 975937182  # Fantastical
-mas install 904280696  # Things
-mas install 747648890  # Telegram
-mas install 439697913  # Icon Slate
-mas install 880001334  # Reeder
-mas install 735066709  # Pixel Winch
-mas install 1289583905 # Pixelmator Pro
 mas install 944693506  # Fileloupe
-mas install 1287239339 # Color Slurp
-mas install 1351639930 # Gifski
 mas install 671595950  # Touch Retouch
-mas install 1522267256 # Shareful
+mas install 1263070803 # Lungo
+mas install 880001334  # Reeder
 
 echo "MAS> Done."
 
@@ -27,8 +24,14 @@ echo "MAS> Done."
 # Optional.
 # -------------------------------------------------------------
 
-# mas install 924691732  # Flare
+# mas install 1522267256 # Shareful
+# mas install 735066709  # Pixel Winch
+# mas install 1287239339 # Color Slurp
+# mas install 439697913  # Icon Slate
+# mas install 1289583905 # Pixelmator Pro
 # mas install 424389933  # Final Cut Pro
+# mas install 1351639930 # Gifski
+# mas install 924691732  # Flare
 
 # -------------------------------------------------------------
 # Archive.
