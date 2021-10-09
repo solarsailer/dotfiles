6c1a153469ce6c29e394df1a0a0b91f3f7ddb4fd
Matthieu Oger <moger@pixelnest.io>
2021-10-09 23:43:29 +0200

    Code: update packages

--- scripts/packages-code.bash
+++ scripts/packages-code.bash
@@ -4,6 +4,9 @@
 # Visual Studio Code.
 # --------------------------------------------------------------
 
+# Before installign the packages below, you need to install the `code` bin.
+# In VSCode, open the command palette and run "Shell Command: Install 'code' command in PATH"
+
 echo "Code> Installing…"
 
 code --install-extension formulahendry.auto-rename-tag # Still necessary despite VSCode own implementation
@@ -15,11 +18,15 @@ code --install-extension pnp.polacode
 code --install-extension esbenp.prettier-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 
+echo "Code> Done."
+
+# -------------------------------------------------------------
+# Archive.
+# -------------------------------------------------------------
+
 # code --install-extension Unity.unity-debug
 # code --install-extension ms-vscode.csharp
 # code --install-extension cmstead.jsrefactor
 # code --install-extension eamodio.gitlens
 # code --install-extension alefragnani.Bookmarks
 # code --install-extension peterjausovec.vscode-docker
-
-echo "Code> Done."
