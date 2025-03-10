c5f5883a7107b342c82c1c8a9deb32fe279a6284
Matthieu Oger <moger@solarsailer.net>
2025-03-10 10:15:17 +0100

    Fish: add flag to brew_info_installed_pkg

--- fish/.config/fish/functions/brew_info_installed_pkg.fish
+++ fish/.config/fish/functions/brew_info_installed_pkg.fish
@@ -1,3 +1,11 @@
 function brew_info_installed_pkg --description "Show info about an already installed package"
-  brew list | gum filter | xargs brew info
+  argparse 'cask' 'no-cask' -- $argv
+
+  if      set --query _flag_no_cask
+    brew list --formula | gum filter | xargs brew info
+  else if set --query _flag_cask
+    brew list --cask    | gum filter | xargs brew info
+  else
+    brew list           | gum filter | xargs brew info
+  end
 end
