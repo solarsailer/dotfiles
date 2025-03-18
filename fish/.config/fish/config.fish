024656c95df464c782a432cca5aefeb7aeff5795
Matthieu Oger <moger@solarsailer.net>
2025-03-18 09:45:33 +0100

    Fish: add auto-added section

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -110,3 +110,11 @@ starship init fish | source
 function hack_starship_add_newline --on-event fish_postexec
   if test "$argv[1]" != 'clear'; echo;  end
 end
+
+# --------------------------------------------------------------
+# Automatically added.
+# --------------------------------------------------------------
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
