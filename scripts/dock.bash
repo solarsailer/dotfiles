ca7e7a497fdf9424ac86ab5a2dfbf13dacf863c0
Matthieu Oger <moger@solarsailer.net>
2025-04-22 21:38:08 +0200

    Dock: change apps

--- scripts/dock.bash
+++ scripts/dock.bash
@@ -13,11 +13,11 @@ dockutil --add "/Applications/Safari.app"
 dockutil --add "/Applications/Google Chrome.app"
 dockutil --add "/Applications/Spotify.app"
 dockutil --add "/Applications/Fantastical 2.app"
+dockutil --add ~/Applications/Fastmail.app
 dockutil --add "/Applications/Messages.app"
 dockutil --add "/Applications/Things3.app"
-dockutil --add "/Applications/Craft.app"
-dockutil --add "/Applications/Notes.app"
-dockutil --add "/Applications/iTerm.app"
+docktuil --add "/Applications/Obsidian.app"
+dockutil --add "/Applications/Ghostty.app"
 
 dockutil --add '~/Downloads' --sort datemodified --view auto --display stack
 
