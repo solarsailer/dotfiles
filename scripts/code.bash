751fa2134ecf7968e239fe8b006cacd53d30ecd2
Matthieu Oger <moger@solarsailer.net>
2025-05-23 15:56:20 +0200

    Code: clean extensions manifest

--- scripts/code.bash
+++ scripts/code.bash
@@ -9,21 +9,44 @@
 
 echo "Code> Installing…"
 
+# Basics.
 code --install-extension formulahendry.auto-rename-tag # Still necessary despite VSCode own implementation
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension tamasfe.even-better-toml
 code --install-extension file-icons.file-icons
 code --install-extension bmalehorn.vscode-fish
-code --install-extension github.github-vscode-theme
-code --install-extension esbenp.prettier-vscode
-code --install-extension bradlc.vscode-tailwindcss
-code --install-extension styled-components.vscode-styled-components
 code --install-extension waderyan.gitblame
+code --install-extension github.github-vscode-theme
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension dbankier.vscode-quick-select
+code --install-extension sapegin.reveal-in-ghostty
+
+# Copilot
+code --install-extension GitHub.copilot
+code --install-extension GitHub.copilot-chat
+
+# Python:
+code --install-extension charliermarsh.ruff
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.debugpy
+# code --install-extension njpwerner.autodocstring
+# code --install-extension KevinRose.vsc-python-indent
 
 # Laravel / PHP:
 # code --install-extension bmewburn.vscode-intelephense-client
 # code --install-extension laravel.vscode-laravel
 # code --install-extension shufo.vscode-blade-formatter
+
+# Style-components:
+# code --install-extension styled-components.vscode-styled-components
+
+# Tailwind:
+# code --install-extension bradlc.vscode-tailwindcss
+
+# Deno:
+# code --install-extension denoland.vscode-deno
 
 echo "Code> Done."
 
@@ -32,7 +55,7 @@ echo "Code> Done."
 # -------------------------------------------------------------
 
 # Useful in case:
-# code --install-extension pnp.polacode
+# code --install-extension adpyke.codesnap
 
 # Deprecated:
 # code --install-extension CoenraadS.bracket-pair-colorizer
@@ -41,6 +64,5 @@ echo "Code> Done."
 # code --install-extension Unity.unity-debug
 # code --install-extension ms-vscode.csharp
 # code --install-extension cmstead.jsrefactor
-# code --install-extension eamodio.gitlens
 # code --install-extension alefragnani.Bookmarks
 # code --install-extension peterjausovec.vscode-docker
